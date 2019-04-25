b30ba100b4bbccee2f7e3c1fe3c498967feb1b8a
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-04-25 13:14:22 -0400

    Initial upload

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+### Schema
+
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id INT AUTO_INCREMENT NOT NULL ,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+SELECT * From burgers
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Barbecue Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Angry WHOPPER®');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Sky Burger', true);
